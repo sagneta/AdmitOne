d7cdffde85fee97b5f0beb8b7902d150d4d4e78f
Stephen Agneta <you@example.com>
2017-01-02 08:34:52 -0500

    Basic working client build. Need to hook up to root.

---
+++ client/installdep.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo npm install --global browserify
+sudo npm install --global babel-cli
+sudo npm install --save-dev react
+sudo npm install --save-dev react-dom
+sudo npm install --save-dev babel-preset-react
+sudo npm install --save-dev babel-preset-es2015
