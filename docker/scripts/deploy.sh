4d85699205fe9a5216df3d7df3dd375938a8189c
Stephen Agneta <you@example.com>
2017-01-03 15:12:16 -0500

    More docker stuff.

--- docker/scripts/deploy.sh
+++ docker/scripts/deploy.sh
@@ -17,8 +17,16 @@ if [ -d $CODEDIR ]; then
     pg_restore -Fc --clean -U bjondhealth -h db -d bjondhealth $DB_DUMP || echo "Dont exit if restore has errors, it always does" 
   fi
   # Build, migrate and deploy app
+  npm install --save-dev browserfy
+  npm install --save-dev babel-cli
+  npm install --save-dev react
+  npm install --save-dev react-dom
+  npm install --save-dev babel-preset-react
+  npm install --save-dev babel-preset-es2015
+  npm install --save-dev react-button 
+  
+  gradle flywayclean flywayMigrate
   gradle
-  gradle flywayMigrate
   gradle deployroot
   # Start wildfly
   $JBOSS_HOME/bin/standalone.sh -b 0.0.0.0
